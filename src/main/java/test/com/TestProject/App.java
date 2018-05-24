e2c10b69727b046784674102c4459526c9467dc3
SaurabDey <saurab.jeet@gmail.com>
2018-05-24 09:25:26 +0530

    First Commit

---
+++ src/main/java/test/com/TestProject/App.java
@@ -0,0 +1,14 @@
+package test.com.TestProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/test/com/TestProject/Saurab.java
@@ -0,0 +1,10 @@
+package test.com.TestProject;
+
+public class Saurab {
+
+	public static void main(String[] args) {
+		System.out.println( "Hello Selenium!" );
+
+	}
+
+}
