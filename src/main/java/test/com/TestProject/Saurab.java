20b804cc6dc0eab5b975eb05012cb3e9686bc30a
SaurabDey <saurab.jeet@gmail.com>
2018-05-25 08:50:58 +0530

    Third commit

--- src/main/java/test/com/TestProject/Saurab.java
+++ src/main/java/test/com/TestProject/Saurab.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class Saurab {
 
@@ -15,6 +16,12 @@ public class Saurab {
 			driver= new  ChromeDriver();
 			driver.get("http://output.jsbin.com/usidix/1");
 			driver.manage().window().maximize();
+		}
+		
+		@Test
+		public void f()
+		{
+			
 		}
 
 		@AfterTest
